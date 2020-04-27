cccfffd3ef1e383f559e15a13ce85fa14fde88d4
Tim Yardley <yardley@gmail.com>
2020-04-27 15:29:25 -0500

    theming and transitions

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['~/assets/main.css'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -54,16 +54,12 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: '#00476B',
+          accent: '#6D6E71',
+          secondary: '#395C23'
         }
       }
     }
