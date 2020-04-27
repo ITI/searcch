5119d47366c922409664505a494af146ffd82a23
Tim Yardley <yardley@gmail.com>
2020-04-27 15:34:40 -0500

    represent the portal

--- pages/index.vue
+++ pages/index.vue
@@ -3,57 +3,46 @@
     <v-flex xs12 sm8 md6>
       <div class="text-center">
         <logo />
-        <vuetify-logo />
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome to the SEARCCH Portal
         </v-card-title>
         <v-card-text>
           <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
+            SEARCCH: Sharing Expertise and Artifacts for Reuse through a
+            Cybersecurity Community Hub. SEARCCH is creating a collaborative,
+            community-driven platform that lowers the barrier to sharing by
+            aiding researchers in packaging, importing, locating, understanding,
+            and reusing experiment artifacts. The artifacts organized by the
+            platform, including tools, methodologies, documentation, and data,
+            can be deployed to community testbeds for performing new
+            experiments. Concurrently, SEARCCH engages in outreach activities to
+            build an active, diverse, online community around the hub to enable
+            direct sharing of expertise and crowdsourcing research ideas and
+            experiment design. Through a process of continuous learning and
+            improvement, the hub will grow over time to include collections of
+            artifacts covering a broad range of cybersecurity research topics
+            and add support for newly identified community needs. These
+            activities together will transform the way cybersecurity research is
+            conducted in the cybersecurity community, improving the overall
+            scientific quality of cybersecurity research through validation,
+            repeatable sharing and reuse, and a collective approach to building
+            on research results.
           </p>
           <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-              discord </a
+            For more information on SEARCCH, check out the
+            <a href="https://searcch.cyberexperimentation.org" target="_blank">
+              project homepage </a
             >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
           </p>
           <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            <em><small>&mdash; SEARCCH Team</small></em>
           </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">
-            Nuxt Documentation
-          </a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-            Nuxt GitHub
-          </a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">
+          <v-btn color="primary" nuxt to="/search">
             Continue
           </v-btn>
         </v-card-actions>
@@ -64,12 +53,10 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Logo
   }
 }
 </script>
