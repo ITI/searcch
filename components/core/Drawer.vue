f293ec514058e1247201f977388baa73533e292f
Tim Yardley <yardley@gmail.com>
2020-05-08 16:08:53 -0500

    feat: bring in free admin template for potential use, mostly integrated and required some fairly significant tweaking

---
+++ components/core/Drawer.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-navigation-drawer
+    id="app-drawer"
+    v-model="inputValue"
+    :src="image"
+    app
+    color="grey darken-2"
+    dark
+    floating
+    mobile-break-point="991"
+    persistent
+    width="260"
+  >
+    <template v-slot:img="attrs">
+      <v-img
+        v-bind="attrs"
+        gradient="to top, rgba(0, 0, 0, .7), rgba(0, 0, 0, .7)"
+      />
+    </template>
+
+    <v-list-item two-line>
+      <v-list-item-title class="title">
+        SEARCCH Admin
+      </v-list-item-title>
+    </v-list-item>
+
+    <v-divider class="mx-3 mb-3" />
+
+    <v-list nav>
+      <!-- Bug in Vuetify for first child of v-list not receiving proper border-radius -->
+      <div />
+
+      <v-list-item
+        v-for="(link, i) in links"
+        :key="i"
+        :to="link.to"
+        active-class="primary white--text"
+      >
+        <v-list-item-action>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-item-action>
+
+        <v-list-item-title v-text="link.text" />
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <v-list nav>
+        <v-list-item to="#">
+          <v-list-item-action>
+            <v-icon>mdi-package-up</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-title class="font-weight-light">
+            Something else
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+// Utilities
+import { mapMutations, mapState } from 'vuex'
+
+export default {
+  props: {
+    opened: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: () => ({
+    links: [
+      {
+        to: '/dashboard/',
+        icon: 'mdi-view-dashboard',
+        text: 'Dashboard'
+      },
+      {
+        to: '/dashboard/user-profile',
+        icon: 'mdi-account',
+        text: 'User Profile'
+      },
+      {
+        to: '/dashboard/table-list',
+        icon: 'mdi-clipboard-outline',
+        text: 'Table List'
+      },
+      {
+        to: '/dashboard/typography',
+        icon: 'mdi-format-font',
+        text: 'Typography'
+      },
+      {
+        to: '/dashboard/icons',
+        icon: 'mdi-chart-bubble',
+        text: 'Icons'
+      },
+      {
+        to: '/dashboard/maps',
+        icon: 'mdi-map-marker',
+        text: 'Maps'
+      },
+      {
+        to: '/dashboard/notifications',
+        icon: 'mdi-bell',
+        text: 'Notifications'
+      }
+    ]
+  }),
+
+  computed: {
+    ...mapState('app', ['image', 'color']),
+    inputValue: {
+      get() {
+        return this.$store.state.app.drawer
+      },
+      set(val) {
+        this.setDrawer(val)
+      }
+    }
+  },
+
+  methods: {
+    ...mapMutations('app', ['setDrawer', 'toggleDrawer'])
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,4 +1,11 @@
 export const state = () => ({
-  ZENODO_API_URL: process.env.ZENODO_API_URL || 'https://zenodo.org/api/',
-  ZENODO_API_KEY: process.env.ZENODO_API_KEY || ''
+	ZENODO_API_URL: process.env.ZENODO_API_URL || 'https://zenodo.org/api/',
+	ZENODO_API_KEY: process.env.ZENODO_API_KEY || '',
+	sidebar: false,
 })
+
+export const mutations = {
+	toggleSidebar(state) {
+		state.sidebar = !state.sidebar
+	},
+}
