753aad0665b12986ca29dc94e9bcdf58c67dac95
Tim Yardley <yardley@gmail.com>
2020-05-11 09:03:42 -0500

    chore: refactor zenodo artifact display

--- components/ZenodoArtifactList.vue
+++ components/ZenodoArtifactList.vue
@@ -1,62 +1,26 @@
 <template>
-  <div v-if="artifacts">
+  <div>
     <div v-for="artifact in artifacts" :key="artifact.id">
-      <v-card class="mx-auto overflow-hidden">
-        <v-card-title class="align-start">
-          <div>
-            <span class="headline">{{ artifact.title | titlecase }}</span>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <span
-            class="pl-2 grey--text text--darken-2 font-weight-light caption"
-          >
-            42 reviews
-          </span>
-          <v-spacer></v-spacer>
-          <v-rating
-            :value="4.5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="18"
-          ></v-rating>
-        </v-card-text>
-
-        <v-card-text v-html="artifact.metadata.description"> </v-card-text>
-
-        <v-card-actions>
-          <v-btn icon>
-            <v-icon>mdi-heart-outline</v-icon>
-          </v-btn>
-
-          <v-btn icon :to="`/artifact/comment/${artifact.id}`" nuxt>
-            <v-icon>mdi-comment</v-icon>
-          </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-            small
-            replace
-            color="info"
-            :to="`/artifact/${artifact.id}`"
-            nuxt
-            target="_blank"
-          >
-            Read More
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <ZenodoArtifactShort
+        :id="artifact.id"
+        :title="artifact.title"
+        :description="artifact.metadata.description"
+      ></ZenodoArtifactShort>
     </div>
   </div>
 </template>
 
 <script>
+import ZenodoArtifactShort from '@/components/ZenodoArtifactShort'
 export default {
+  components: {
+    ZenodoArtifactShort
+  },
   props: {
-    artifacts: Array
+    artifacts: {
+      type: Array,
+      required: true
+    }
   }
 }
 </script>
---
+++ components/ZenodoArtifactShort.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <v-card class="mx-auto overflow-hidden">
+      <v-card-title class="align-start">
+        <div>
+          <span class="headline">{{ title | titlecase }}</span>
+        </div>
+      </v-card-title>
+      <v-card-text>
+        <span class="pl-2 grey--text text--darken-2 font-weight-light caption">
+          42 reviews
+        </span>
+        <v-spacer></v-spacer>
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="18"
+        ></v-rating>
+      </v-card-text>
+
+      <v-card-text v-html="description"> </v-card-text>
+
+      <v-card-actions>
+        <v-btn icon>
+          <v-icon>mdi-heart-outline</v-icon>
+        </v-btn>
+
+        <v-btn icon :to="`/artifact/comment/${id}`" nuxt>
+          <v-icon>mdi-comment</v-icon>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          small
+          replace
+          color="info"
+          :to="`/artifact/${id}`"
+          nuxt
+          target="_blank"
+        >
+          Read More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
