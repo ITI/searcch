e726b1db318e28ed53788ac596dfb82dd8bc91be
Tim Yardley <yardley@gmail.com>
2020-08-11 19:43:28 -0500

    chore: shift around the display a little

--- components/ArtifactShort.vue
+++ components/ArtifactShort.vue
@@ -1,47 +1,44 @@
 <template>
   <div>
     <v-card class="mx-auto overflow-hidden">
+      <v-chip
+        v-if="artifact.relevance_score"
+        :color="relevanceColor"
+        class="ma-2"
+        label
+      >
+        <v-avatar left>
+          <v-icon>mdi-finance</v-icon>
+        </v-avatar>
+        {{ artifact.relevance_score }}
+      </v-chip>
+
+      <v-chip v-if="artifact.type" color="primary" class="ma-2" label>
+        <v-avatar left>
+          <v-icon>mdi-newspaper-variant-outline</v-icon>
+        </v-avatar>
+        <div v-if="artifact.type">{{ artifact.type }}</div>
+      </v-chip>
+
       <v-card-title class="align-start">
         <div>
           <span class="headline">{{ artifact.title | titlecase }}</span>
         </div>
       </v-card-title>
-      <v-card-text>
-        <span class="pl-2 grey--text text--darken-2 font-weight-light caption">
-          {{ reviews }} reviews
-        </span>
-        <v-spacer></v-spacer>
-        <v-rating
-          v-model="rating"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="18"
-        ></v-rating>
-      </v-card-text>
 
-      <v-card-text v-if="artifact.relevance_score">
-        <v-card-title> Relevance Score </v-card-title>
+      <v-spacer></v-spacer>
 
-        <v-chip :color="relevanceColor" class="ma-2" label>
-          <v-avatar left>
-            <v-icon>mdi-finance</v-icon>
-          </v-avatar>
-
-          {{ artifact.relevance_score }}
-        </v-chip>
-
-        <v-card-title> Artifact Type </v-card-title>
-
-        <v-chip color="primary" class="ma-2" label>
-          <v-avatar left>
-            <v-icon>mdi-newspaper-variant-outline</v-icon>
-          </v-avatar>
-
-          <div v-if="artifact.type">{{ artifact.type }}</div>
-        </v-chip>
-      </v-card-text>
+      <span class="pl-2 grey--text text--darken-2 font-weight-light caption">
+        {{ reviews }} reviews
+      </span>
+      <v-rating
+        v-model="rating"
+        color="amber"
+        dense
+        half-increments
+        readonly
+        size="18"
+      ></v-rating>
 
       <v-card-text v-html="sanitizedDescription"> </v-card-text>
 
