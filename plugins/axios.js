4360c5e39e556c393d564c43151babf22d60f505
Tim Yardley <yardley@gmail.com>
2021-07-13 16:05:04 -0500

    fix: only add authentication header to items we are proxying to our backend

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,7 @@
 export default function({ $axios, store }) {
   $axios.onRequest(config => {
-    if (store.state.user.user_token) {
+    if (store.state.user.user_token && config.url.startsWith('/kg/')) {
+      // console.log('added header to url: ' + config.url)
       config.headers.common['Authorization'] = store.state.user.user_token
     }
   })
