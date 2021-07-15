d3fcde9803f7fbc46455c3e35c1e51464878d3a5
Tim Yardley <yardley@gmail.com>
2021-07-14 22:16:48 -0500

    fix: eliminate scroll behavior warning

--- nuxt.config.js
+++ nuxt.config.js
@@ -157,14 +157,7 @@ export default {
   },
   router: {
     // middleware: 'auth'
-    mode: 'history',
-    scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-        return savedPosition
-      } else {
-        return { x: 0, y: 0 }
-      }
-    }
+    mode: 'history'
   },
   auth: {
     strategies: {
