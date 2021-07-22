f25279259f00bf26f140e6ddeac193ec1cb93466
Tim Yardley <yardley@gmail.com>
2021-07-22 14:14:46 -0500

    feat: bring in helper function

--- helpers/index.js
+++ helpers/index.js
@@ -30,3 +30,23 @@ export const bytesToSize = bytes => {
   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
 }
+
+export const convertArrayToObject = (array, key) => {
+  const initialValue = {}
+  return array.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item[key]]: item
+    }
+  }, initialValue)
+}
+
+export const zipArray = (tag, input) => {
+  let zip = [[tag]]
+  input.map(e => zip.push([e]))
+  const mapWith = keys => values =>
+      Object.fromEntries(keys.map((k, i) => [k, values[i]])),
+    getArray = ([keys, ...zip]) => zip.map(mapWith(keys)),
+    array = getArray(zip)
+  return array
+}
