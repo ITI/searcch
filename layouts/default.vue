fd89f9c439217acda91e060fe8d0890b3521022f
Tim Yardley <yardley@gmail.com>
2021-08-12 09:41:50 -0500

    feat: make user login a single click

--- layouts/default.vue
+++ layouts/default.vue
@@ -98,7 +98,7 @@
       <v-btn v-if="$auth.loggedIn" class="primary" @click="logout()"
         >Logout</v-btn
       >
-      <v-btn v-else class="primary" nuxt to="/login">Login</v-btn>
+      <v-btn v-else class="primary" nuxt @click="login()">Login</v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -218,6 +218,9 @@ export default {
     }
   },
   methods: {
+    async login() {
+      let response = await this.$auth.loginWith('github')
+    },
     async logout() {
       if (confirm('Log out of SEARCCH?')) {
         console.log('Logging out')
