a8dcde37caa3bab7fe413fdccf7538efc767f109
Laura S Tinnel <laura.tinnel@sri.com>
2021-10-29 12:14:15 -0400

    Update faqs.vue
    Additional fixes for clarification.

--- pages/faqs.vue
+++ pages/faqs.vue
@@ -60,14 +60,14 @@ export default {
 	  a: 'While SEARCCH is a catalog and search engine for artifacts stored in disparate locations across the Internet, it does not automatically crawl the Internet as do other search engines. Artifacts must be submitted to the hub for import. The SEARCCH team pre-populated the catalog with many artifacts. The working model, however, is for researchers to submit their own artifacts for cataloging. Researchers should first check to see if older artifacts are in the hub and then submit if they are not found.'
 	},
         {
-          q: 'How can I submit my data or code to the SEARCCH catalog?',
+          q: 'How can I submit my artifacts to the SEARCCH catalog?',
           a:
-            'Use the SEARCCH importer service to create and import artifact metadata into the SEARCCH catalog. Please note that SEARCCH does not store the actual artifacts. Artifacts must be published on the internet with a valid URL to be imported into the hub.<br/><br/>SEARCCH supports assisted import and fully manual import.  Artifacts published on github, ACM digital library, ieeexplore, usenix web site publication, arxiv, paperswithcode, Zenodo, and generic git repositories reachable from https can be imported using the assisted import function (the default). The import assistant will scan artifacts in these repositories and automatically populate the import web form for user review. The user can accept the field contents as is or make any modifications desired. Artifacts located in other online locations such as on personal websites can be manually imported by the user by entering all the fields required. Entered data may be saved iteratively by clicking SAVE.<br/><br/>Please see the <a href="https://searcch.cyberexperimentation.org/best-practices">best practices and considerations for SEARCCH hub curation</a>, and then visit <a href="https://hub.cyberexperimentation.org/importer">https://hub.cyberexperimentation.org/importer</a> to start importing your work.'
+            'Use the SEARCCH importer service to create and import data and code artifact metadata into the SEARCCH catalog. Please note that SEARCCH does not store the actual artifacts. Artifacts must be published on the internet with a valid URL to be imported into the hub.<br/><br/>SEARCCH supports assisted import and fully manual import.  Artifacts published on github, ACM digital library, ieeexplore, usenix web site publication, arxiv, paperswithcode, Zenodo, and generic git repositories (reachable using http)s can be imported using the assisted import function (the default). The import assistant will scan artifacts in these repositories and automatically populate the import web form for user review. The user can accept the field contents as is or make any modifications desired.<br/><br/>Artifacts stored on other online locations (e.g., on personal websites) can be manually imported by entering all the fields required. Entered data may be saved iteratively by clicking "SAVE".<br/><br/>Once artifact metadata has been confirmed as being correct, click "PUBLISH" to publish the artifact information in the catalog.<br/><br/>Please see the <a href="https://searcch.cyberexperimentation.org/best-practices">best practices and considerations for SEARCCH hub curation</a>, and then visit <a href="https://hub.cyberexperimentation.org/importer">https://hub.cyberexperimentation.org/importer</a> to start importing your work.'
         },
         {
           q: 'Do I have to share artifacts in order to use the hub?',
           a:
-            'No. Researchers are free to browse the SEARCCH hub contents to find and access useful artifacts in support of their own research. It is our hope that, over time, researchers will generate artifacts that have broader use, make them freely available on the Internet, and index them in the hub so others can quickly find and reuse them.'
+            'No. Researchers are free to browse the SEARCCH hub contents to find and access useful artifacts in support of their own research. It is our hope that over time, researchers will generate artifacts that have broader use, make them freely available on the Internet, and index them in the SEARCCH catalog so others can quickly find and reuse them.'
         },
 	{
           q: 'Is there any cost to use the hub?',
@@ -82,7 +82,7 @@ export default {
         {
           q: 'How can I get an account on the SEARCCH hub?',
           a:
-            'SEARCCH currently leverages single sign-on from GitHub.com. To sign in, click on “LOGIN” in the upper right hand corner of the SEARCCH hub and log in using your GitHub account. If you do not have a GitHub account, please visit <a href="https://github.com/signup" target="_blank" rel="noopener">https://github.com/signup</a> to sign up for an account.  SEARCCH may, in the future, use additional single sign-on sources.'
+            'SEARCCH currently leverages single sign-on from GitHub.com. To sign in, click on “LOGIN” in the upper right hand corner and log in using your GitHub account. If you do not have a GitHub account, please visit <a href="https://github.com/signup" target="_blank" rel="noopener">https://github.com/signup</a> to sign up for an account.  SEARCCH may, in the future, use additional single sign-on sources.'
         },
         {
           q: 'I have code, data, and other artifacts that are all from the same research. How should I submit those to the hub?',
@@ -109,28 +109,28 @@ export default {
           q:
             'I found a tool in the Hub, but the tool doesn’t work or works incorrectly. How do I get help or submit feedback?',
           a:
-            'Please do not submit feedback if you are having issues with using a tool. The SEARCCH team does not have direct experience with most of the artifacts that are in the system and will be unable to help you. <br/><br/> It is always best to first contact the author and try to work out any issues or misunderstandings. Use the SEARCCH entry to identify the tool’s owner/author, and contact the owner directly for tool support. If you still have issues with the tool after reaching out for support, you can try asking a question using the review tool and leaving your contact info there. <br/><br/>You can also share your experience with the tool by leaving a review of the artifact in the hub. Please try to be constructive, understanding that most tools are not products; they are typically shared for use “as is”. If the tool is not maintained, leaving a review that says so and giving the artifact a low rating can help others. We also encourage sharing positive experiences.'
+            'Please do not submit feedback if you are having issues with a tool. The SEARCCH team does not have direct experience with most of the artifacts that are in the system and will be unable to provide assistance. <br/><br/> First contact the author and try to work out any issues or misunderstandings. Use the SEARCCH entry to identify the tool’s owner/author, and contact the owner directly for tool support. If you still have issues with the tool after reaching out for support, you can try asking a question using the review tool and leaving your contact info there. <br/><br/>You can also share your experience with the tool by leaving a review of the artifact in the hub. Please try to be constructive, understanding that most tools are not products; they are typically shared for use “as is”. If the tool is not maintained, leaving a review that says so and giving the artifact a low rating can help others. We also encourage sharing positive experiences.'
         },
         {
           q:
             'I found a dataset in the Hub, but the data is wrong, malformed, or mislabeled. How do I get help or submit feedback?',
           a:
-            'Please do not submit feedback if you are having issues with a dataset. The SEARCCH team does not have direct experience with most of the artifacts that are in the system and will be unable to help you. <br/><br/> It is always best to first contact the author and try to work out any issues or misunderstandings. Use the SEARCCH entry to identify the dataset’s owner/author, and contact the owner directly for support. If you still have issues with the dataset after reaching out for support, you can try asking a question using the review feature. Be sure to leave your contact information so folks can reach out to you. <br/><br/> You can also share your experience using the data by leaving a review of the artifact in the hub. Please try to be constructive in your comments. E.g., if you found a way to fix broken labeling and were then able to use the dataset, share how to do that. We also encourage sharing positive experiences.'
+            'Please do not submit feedback if you are having issues with a dataset. The SEARCCH team does not have direct experience with most of the artifacts that are in the system and will be unable to provide assistance. <br/><br/> First contact the author and try to work out any issues or misunderstandings. Use the SEARCCH entry to identify the dataset’s owner/author, and contact the owner directly for support. If you still have issues with the dataset after reaching out for support, you can try asking a question using the review feature. Be sure to leave your contact information so folks can reach out to you. <br/><br/> You can also share your experience using the data by leaving a review of the artifact in the hub. Please try to be constructive in your comments. E.g., if you found a way to fix broken labeling and were then able to use the dataset, share how to do that. We also encourage sharing positive experiences.'
         },
         {
           q: 'How do I add a profile picture?',
           a:
-            'Direct profile picture upload is not yet implemented in the hub. The hub automatically uses your GitHub avatar, if it is set. If GitHub does not return an avatar for your account, the hub queries gravatar.com for profile pictures associated with your registered SEARCCH email address. <br/><br/> If you would like your profile picture to show up in the SEARCCH hub, either set an avatar image on GitHub.com by visiting <a href="https://github.com/settings/profile" target="_blank" rel="noopener">https://github.com/settings/profile</a> or go to <a href="https://gravatar.com" target="_blank" rel="noopener">https://gravatar.com</a> and register (or add) the email address you used for SEARCCH.'
+            'Direct profile picture upload is not yet supported. The hub automatically uses your GitHub avatar. If you do not have a GitHub avatar, the hub queries gravatar.com for profile pictures associated with your registered SEARCCH email address. <br/><br/> If you would like your profile picture to show up in the SEARCCH hub, either set an avatar image on GitHub.com by visiting <a href="https://github.com/settings/profile" target="_blank" rel="noopener">https://github.com/settings/profile</a> or go to <a href="https://gravatar.com" target="_blank" rel="noopener">https://gravatar.com</a> and register (or add) the email address you used for SEARCCH.'
+        },
+        {
+          q: 'Who is behind SEARCCH?',
+          a:
+            'SEARCCH is developed under a collaboration between the University of Southern California Information Sciences Institute (USC-ISI), the University of Utah (Utah), the University of Illinois Urbana-Champaign (UIUC), and SRI International (SRI).'
         },
         {
           q: 'How is SEARCCH funded?',
           a:
             'SEARCCH is funded under cooperative grants from the National Science Foundation. Grant numbers are CNS-1925773, CNS-1925616, CNS-1925588, and CNS-1925564.'
-        },
-        {
-          q: 'Who is behind SEARCCH?',
-          a:
-            'SEARCCH is being developed under a collaboration between the University of Southern California Information Sciences Institute (USC-ISI), the University of Utah (Utah), the University of Illinois Urbana-Champaign (UIUC), and SRI International (SRI).'
         }
       ]
     }
