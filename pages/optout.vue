dd64293d4b119031c31b7e436ba8b7b432d4e5c4
Bingji Guo <guobingji@126.com>
2023-03-24 10:50:00 -0500

    WIP: optout page

---
+++ pages/optout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            <div>
+                <h1>We'll miss you ..</h1>
+                <p>
+                You have been opted out of the searcch hub. You will no longer be
+                notified of new searcch results.
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OptOut',
+    mounted() {
+        const payload = {
+            'email': this.$route.query.email,
+            'token': this.$route.query.token
+        }
+        const response = this.$optOutEndpoint.index(payload)
+        // TODO: handle response
+    },
+};
+</script>
