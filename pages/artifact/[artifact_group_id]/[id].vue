3fd2fb9222772e3769d662f56a66297b8a7e491f
Bingji Guo <guobingji@126.com>
2023-05-07 23:12:42 -0500

    fix: add page

--- pages/artifact/[artifact_group_id]/[id].vue
+++ pages/artifact/[artifact_group_id]/[id].vue
@@ -1,1 +1,75 @@
-../_artifact_group_id.vue
+<template>
+  <div>
+    <a @click="$router.go(-1)">Back</a>
+
+    <div v-if="artifact.artifact">
+      <div v-if="editing">
+        <LazyHydrate when-visible>
+          <KGArtifactEdit :record="artifact"></KGArtifactEdit>
+        </LazyHydrate>
+      </div>
+      <div v-else-if="editingRelation">
+        <LazyHydrate when-visible>
+          <KGArtifactEditRelation :record="artifact"></KGArtifactEditRelation>
+        </LazyHydrate>
+      </div>
+      <div v-else>
+        <LazyHydrate when-visible>
+          <KGArtifactLong :record="artifact"></KGArtifactLong>
+        </LazyHydrate>
+      </div>
+    </div>
+    <div v-else>{{ loadingMessage }}</div>
+  </div>
+</template>
+  
+<script>
+import { defineAsyncComponent } from 'vue'
+import { mapState } from 'pinia'
+import { artifactsStore } from '~/stores/artifacts'
+import KGArtifactEditRelation from '~/components/KGArtifactEditRelation.vue'
+
+export default defineComponent({
+  components: {
+    KGArtifactLong: defineAsyncComponent(() => import("@/components/KGArtifactLong")),
+    KGArtifactEdit: defineAsyncComponent(() => import("@/components/KGArtifactEdit")),
+    KGArtifactEditRelation: defineAsyncComponent(() => import("@/components/KGArtifactEditRelation")),
+  },
+  head() {
+    return {
+      title: this.artifact.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.artifact.title
+        }
+      ]
+    }
+  },
+  data() {
+    return {
+      loadingMessage: 'Loading...'
+    }
+  },
+  computed: {
+    ...mapState(artifactsStore, {
+      artifact: state => state.artifact
+    }),
+    editing() {
+      return this.$route.query.edit !== undefined
+        && this.$route.query.edit == 'true' ? true : false
+    },
+    editingRelation() {
+      return this.$route.query.edit_relation !== undefined
+        && this.$route.query.edit_relation == 'true' ? true : false
+    }
+  },
+  mounted() {
+    this.$artifactsStore.fetchArtifact({
+      artifact_group_id: this.$route.params.artifact_group_id,
+      id: this.$route.params.id
+    })
+  }
+});
+</script>
