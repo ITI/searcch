47bd3b3a80d6895438061c654b70b0afa934fc30
Bingji Guo <guobingji@126.com>
2023-08-10 23:43:34 -0500

    chore: more tests...

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
-describe('Home Page', () => {
+describe('Home Page for Guests', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit(`${Cypress.env('baseUrl')}`)
     // sometimes we have slow network, run this to wait for the full page to load
     cy.get('nav a[href="/search"]').should('be.visible')
   })
@@ -38,8 +38,7 @@ describe('Home Page', () => {
   context('sidebar', () => {
     it('should have a sidebar with appropriate links', () => {
       cy.get('nav').should('be.visible')
-      cy.get('nav a').as('navItems').should('have.length', 4)
-
+      
       const navAssertions = [
         {href: '/search', target: undefined, title: 'Search Artifacts'},
         {href: '/', target: undefined, title: 'About'},
@@ -47,6 +46,7 @@ describe('Home Page', () => {
         {href: '/faqs', target: undefined, title: 'FAQ'}
       ]
 
+      cy.get('nav a').as('navItems').should('have.length', navAssertions.length)
       navAssertions.forEach(({href, target, title}, index) => {
         if (target === '_blank' && href.startsWith('http')) {
           cy.get('@navItems').eq(index)
@@ -55,7 +55,7 @@ describe('Home Page', () => {
             .contains(title)
         } else {
           cy.get('@navItems').eq(index).contains(title).click()
-          cy.url().should('eq', `http://localhost:3000${href}`)
+          cy.url().should('eq', `${Cypress.env('baseUrl')}${href}`)
         }
         if (title === 'About') {
           cy.get('@navItems').eq(index).should('have.class', 'v-list-item--active')
@@ -87,7 +87,65 @@ describe('Home Page', () => {
         .should('contain', 'Continue')
         .and('be.visible')
         .click()
-      cy.url().should('eq', 'http://localhost:3000/search')
+      cy.url().should('eq', `${Cypress.env('baseUrl')}/search`)
     })
   })
+})
+
+describe('Home page for logged in users', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env('baseUrl')}`)
+    cy.get('nav a[href="/search"]').should('be.visible')
+    cy.login()
+  })
+
+  context('header', () => {
+    it('display username', () => {
+      cy.window().then((win) => {
+        cy.get('header span').contains(win.useNuxtApp().$auth.user.name)
+      })
+    })
+
+    it('can logout', () => {
+      cy.get('header button').last().contains('Logout').click()
+      cy.get('header button').last().contains('Login')
+    })
+  })
+
+  context('sidebar', () => {
+    it('should have a sidebar with appropriate links', () => {
+      cy.get('nav').should('be.visible')
+
+      const navAssertions = [
+        {href: '/search', target: undefined, title: 'Search Artifacts'},
+        {href: '/favorites', target: undefined, title: 'Favorite Artifacts'},
+        {href: '/myartifacts', target: undefined, title: 'My Artifacts'},
+        {href: '/import', target: undefined, title: 'Submit Artifact'},
+        {href: '/profile', target: undefined, title: 'Manage Account'},
+        {href: '/', target: undefined, title: 'About'},
+        {href: 'https://searcch.cyberexperimentation.org/best-practices', target: '_blank', title: 'Best Practices'},
+        {href: '/faqs', target: undefined, title: 'FAQ'}
+      ]
+
+      cy.get('nav a').as('navItems').should('have.length', navAssertions.length)
+      navAssertions.forEach(({href, target, title}, index) => {
+        if (target === '_blank' && href.startsWith('http')) {
+          cy.get('@navItems').eq(index)
+            .should('have.attr', 'target', target)
+            .and('have.attr', 'href', href)
+            .contains(title)
+        } else {
+          cy.get('@navItems').eq(index).contains(title).click()
+          cy.url().should('eq', `${Cypress.env('baseUrl')}${href}`)
+        }
+        if (title === 'About') {
+          cy.get('@navItems').eq(index).should('have.class', 'v-list-item--active')
+        }
+      })
+    })
+  })
+})
+
+describe('Home page for admins', () => {
+
 })
--- cypress/e2e/search-and-filter-artifacts.cy.js
+++ cypress/e2e/search-and-filter-artifacts.cy.js
@@ -1,5 +1,102 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Search and Filter Artifacts for Guests', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env('baseUrl')}/search`)
+    cy.get('nav a[href="/search"]').should('be.visible')
   })
+
+  it('displays a search bar', () => {
+    cy.get('#input-search-artifact').should('exist')
+  })
+
+  it('can toggle advanced search', () => {
+    cy.get('.mdi-chevron-down').should('be.visible')
+
+    cy.get('.v-expansion-panel-title__overlay').click()
+    cy.get('.v-expansion-panel-text__wrapper').should('be.visible')
+
+    cy.get('.v-expansion-panel-title__overlay').click()
+    cy.get('.v-expansion-panel-text__wrapper').should('not.exist')
+  })
+
+  context('can display search results', () => {
+    it('can navigate using pagination', () => {
+      cy.get('#input-search-artifact').type('{enter}')
+      cy.get('.v-pagination__list button').should('have.length.at.least', 2)
+      
+      cy.get('.v-card').should('have.length.at.least', 1)
+      cy.get('.v-card').first().as('firstCard')
+
+      cy.intercept('GET', '/kg/artifact/search?*').as('getArtifacts')
+      cy.get('.v-pagination__list button').last().click()
+      cy.wait('@getArtifacts').its('response.statusCode').should('eq', 200)
+
+      cy.get('.v-card').should('have.length.at.least', 1)
+      cy.get('.v-card').first().should('not.equal', '@firstCard')
+    })
+
+    it(' cannot navigate using pagination when limited results exist', () => {
+      cy.get('#input-search-artifact').type('stackdb{enter}')
+      cy.get('.v-pagination__list').should('be.visible')
+      cy.get('.v-pagination__list button').should('have.length', 3)
+      cy.get('.v-card').should('have.length.at.least', 1)
+    })
+
+    it('displays no result when the search is inapplicable', () => {
+      cy.get('#input-search-artifact').type('deadbeef{enter}')
+      cy.get('.v-pagination__list').should('be.visible')
+      cy.get('.v-pagination__list button').should('have.length', 2)
+      cy.get('.v-card').should('have.length', 0)
+    })
+  })
+
+
+  // it('can filter artifacts by author', () => {
+
+  // })
+
+  // it('can filter artifacts by organization', () => {
+
+  // })
+
+  // it('can filter artifacts by artifact type', () => {
+
+  // })
+
+  // it('can filter artifacts by artifact badge', () => {
+
+  // })
+
+  // it('can filter artifacts by artifact venue', () => {
+
+  // })
+
+  // context('can sort artifacts by date', () => {
+  //   it('ascending', () => {
+
+  //   })
+
+  //   it('descending', () => {
+
+  //   })
+  // })
+
+  // context('can sort artifacts by view', () => {
+  //   it('ascending', () => {
+
+  //   })
+
+  //   it('descending', () => {
+
+  //   })
+  // })
+
+  // context('can sort artifacts by rating', () => {
+  //   it('ascending', () => {
+
+  //   })
+
+  //   it('descending', () => {
+
+  //   })
+  // })
 })
