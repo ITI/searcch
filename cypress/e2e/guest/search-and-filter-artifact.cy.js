9092007bf0fd6dae95aca2d069b91bc8dc37f643
Bingji Guo <guobingji@126.com>
2023-08-30 11:40:20 -0500

    fix: make search test backend independent

--- cypress/e2e/guest/search-and-filter-artifact.cy.js
+++ cypress/e2e/guest/search-and-filter-artifact.cy.js
@@ -20,13 +20,13 @@ describe('Search and Filter Artifacts', () => {
 
 	context('can display search results', () => {
 		it('can navigate using pagination', () => {
+			cy.intercept('GET', '/kg/artifact/search?*', { fixture: 'artifacts.json' }).as('getArtifacts')
 			cy.get('#input-search-artifact').type('{enter}')
 			cy.get('.v-pagination__list button').should('have.length.at.least', 2)
 
 			cy.get('.v-card').should('have.length.at.least', 1)
 			cy.get('.v-card').first().as('firstCard')
 
-			cy.intercept('GET', '/kg/artifact/search?*').as('getArtifacts')
 			cy.get('.v-pagination__list button').last().click()
 			cy.wait('@getArtifacts').its('response.statusCode').should('eq', 200)
 
@@ -35,13 +35,23 @@ describe('Search and Filter Artifacts', () => {
 		})
 
 		it('cannot navigate using pagination when limited results exist', () => {
+			cy.fixture('artifacts.json').then((fixture) => {
+				fixture.artifacts.pages = 1
+				fixture.artifacts.total = 10
+				cy.intercept('GET', '/kg/artifact/search?*', fixture).as('getArtifacts')
+			})
 			cy.get('#input-search-artifact').type('stackdb{enter}')
 			cy.get('.v-pagination__list').should('be.visible')
 			cy.get('.v-pagination__list button').should('have.length', 3)
-			cy.get('.v-card').should('have.length.at.least', 1)
 		})
 
 		it('displays no result when the search is inapplicable', () => {
+			cy.fixture('artifacts.json').then((fixture) => {
+				fixture.artifacts.pages = 1
+				fixture.artifacts.total = 0
+				fixture.artifacts.artifacts = []
+				cy.intercept('GET', '/kg/artifact/search?*', fixture).as('getArtifacts')
+			})
 			cy.get('#input-search-artifact').type('deadbeef{enter}')
 			cy.get('.v-pagination__list').should('be.visible')
 			cy.get('.v-pagination__list button').should('have.length', 2)
@@ -51,6 +61,7 @@ describe('Search and Filter Artifacts', () => {
 
 	context('can do action with a search result', () => {
 		beforeEach(() => {
+			cy.intercept('GET', '/kg/artifact/search?*', { fixture: 'artifacts.json' }).as('getArtifacts')
 			cy.get('#input-search-artifact').type('stackdb{enter}')
 			cy.get('.v-card').should('have.length.at.least', 1)
 		})
@@ -75,88 +86,63 @@ describe('Search and Filter Artifacts', () => {
 		beforeEach(() => {
 			cy.get('.v-expansion-panel-title__overlay').click()
 			cy.get('.v-expansion-panel-text__wrapper').should('be.visible')
-			cy.intercept('GET', '/kg/artifact/search?*').as('getArtifacts')
 		})
 
 		it('by author', () => {
-			cy.get('#input-filter-by-author').type('issac newton')
+			const author = 'issac newton'
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`author=${author.replace(' ', '+')}`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
+			cy.get('#input-filter-by-author').type(author)
 			cy.get('#btn-advanced-search-submit').click()
 			cy.wait('@getArtifacts')
-			cy.get('.v-card').should('have.length', 0)
-
-			cy.get('#input-filter-by-author').clear().type('david')
-			cy.get('#btn-advanced-search-submit').click()
-			cy.wait('@getArtifacts')
-			cy.get('.v-card').should('have.length.at.least', 1)
 		})
 
 		it('by organization', () => {
-			cy.get('#input-filter-by-organization').type('University College London')
+			const org = 'University College London'
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`organization=${org.replace(' ', '+')}`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
+			cy.get('#input-filter-by-organization').type(org)
 			cy.get('#btn-advanced-search-submit').click()
 			cy.wait('@getArtifacts')
-			cy.get('.v-card').should('have.length.at.least', 1)
-
-			cy.get('#input-filter-by-organization').clear().type('Hogwarts School of Witchcraft and Wizardry')
-			cy.get('#btn-advanced-search-submit').click()
-			cy.wait('@getArtifacts')
-			cy.get('.v-card').should('have.length', 0)
 		})
 
 		it('by artifact type', { scrollBehavior: false }, () => {
-			cy.get('#input-search-artifact').type('stackdb{enter}')
-			cy.wait('@getArtifacts')
-			cy.get('.v-pagination__list button').then(($btns) => {
-				const length = $btns.length
-
-				cy.get('.v-field__input').eq(3).click()
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(0).click()
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(1).click()
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-				cy.get('.v-pagination__list button').should('not.have.length', length)
-
-				cy.get('.v-field__input').eq(3).click()
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(0).click()
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(0).click()
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(4).click()
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-				cy.get('.v-pagination__list button').should('have.length', length)
-			})
-		})
-
-		it('by artifact badge', { scrollBehavior: false }, () => {
-			cy.get('.v-field__input').eq(4).click({ force: true })
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`type=`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
+			cy.get('.v-field__input').eq(3).click()
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(0).click()
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(1).click()
 			cy.get('#btn-advanced-search-submit').click({ force: true })
 			cy.wait('@getArtifacts')
-			cy.get('.v-pagination__list button').then(($btns) => {
-				const length = $btns.length
+		})
 
-				cy.get('.v-field__input').eq(4).click({ force: true })
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(2).click()
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-				cy.get('.v-pagination__list button').should('not.have.length', length)
-			})
+		it('by artifact badge', { scrollBehavior: false }, () => {
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`badge_id=`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
+			cy.get('.v-field__input').eq(4).click({ force: true })
+			cy.get('.v-overlay:last-of-type .v-list-item').eq(1).click()
+			cy.get('#btn-advanced-search-submit').click({ force: true })
+			cy.wait('@getArtifacts')
 		})
 
 		it('by artifact venue', { scrollBehavior: false }, () => {
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`venue_id=`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
 			cy.get('.v-field__input').eq(5).click({ force: true })
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(0).click()
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(1).click()
 			cy.get('#btn-advanced-search-submit').click({ force: true })
 			cy.wait('@getArtifacts')
-			cy.get('.v-pagination__list button').then(($btns) => {
-				const length = $btns.length
-
-				cy.get('.v-field__input').eq(5).click({ force: true })
-				cy.get('.v-overlay:last-of-type .v-list-item').eq(2).click()
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-				cy.get('.v-pagination__list button').should('not.have.length', length)
-			})
 		})
 	})
 
@@ -168,48 +154,39 @@ describe('Search and Filter Artifacts', () => {
 		})
 
 		it('by date', () => {
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`sort=Date`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
 			cy.get('.v-field__input').eq(6).click({ force: true })
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(1).click({ force: true })
 			cy.get('.v-col-sm-4 input').first().click({ force: true })
 			cy.get('#btn-advanced-search-submit').click({ force: true })
 			cy.wait('@getArtifacts')
-			cy.get('.v-card').first().then(($firstCard) => {
-				cy.get('.v-col-sm-4 input').last().click({ force: true })
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-
-				cy.get('.v-card').first().should('not.equal', $firstCard)
-			})
 		})
 
 		it('by view', () => {
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`sort=View`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
 			cy.get('.v-field__input').eq(6).click({ force: true })
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(2).click({ force: true })
 			cy.get('.v-col-sm-4 input').first().click({ force: true })
 			cy.get('#btn-advanced-search-submit').click({ force: true })
 			cy.wait('@getArtifacts')
-			cy.get('.v-card').first().then(($firstCard) => {
-				cy.get('.v-col-sm-4 input').last().click({ force: true })
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-
-				cy.get('.v-card').first().should('not.equal', $firstCard)
-			})
 		})
 
 		it('by rating', () => {
+			cy.intercept('GET', '/kg/artifact/search?*', (req) => {
+				expect(req.url()).to.include(`sort=Rating`)
+				req.reply({ fixture: 'artifacts.json' })
+			}).as('getArtifacts')
 			cy.get('.v-field__input').eq(6).click({ force: true })
 			cy.get('.v-overlay:last-of-type .v-list-item').eq(3).click({ force: true })
 			cy.get('.v-col-sm-4 input').first().click({ force: true })
 			cy.get('#btn-advanced-search-submit').click({ force: true })
 			cy.wait('@getArtifacts')
-			cy.get('.v-card').first().then(($firstCard) => {
-				cy.get('.v-col-sm-4 input').last().click({ force: true })
-				cy.get('#btn-advanced-search-submit').click({ force: true })
-				cy.wait('@getArtifacts')
-
-				cy.get('.v-card').first().should('not.equal', $firstCard)
-			})
 		})
 	})
 })
