62805bc44178b2011ff7125465bd708cba86a0fa
Bingji Guo <guobingji@126.com>
2023-08-30 11:17:04 -0500

    fix: artifact chip does not display correctly

--- components/ArtifactChips.vue
+++ components/ArtifactChips.vue
@@ -1,78 +1,83 @@
 <template>
-  <div>
-    <v-chip
-      v-if="fieldData.length"
-      v-for="(item, index) in fieldData"
-      :key="`field${index}${Math.random()}`"
-      cols="12"
-      class="ma-2"
-      label
-      :color="iconColor(type, item)"
-      v-bind:closable="edit ? !display : undefined"
-      :close-icon="edit ? 'mdi-close' : undefined"
-      @click:close="onDeleteChip(irem, index)"
-      :to="whereTo(item)"
-      v-bind:small="small"
+  <v-row class="align-center my-2">
+    <v-col class="pa-0" cols="auto"
+      v-if="fields.length"
+      v-for="(item, index) in fields"
+      :key="item.id"
     >
-      <v-avatar start>
-        <v-icon>{{ iconImage(type, item) }}</v-icon>
-      </v-avatar>
-      <div v-if="!isObject(fieldData[index])">
-        <v-text-field
-          v-if="edit"
-          class="m-0 input-chip"
-          variant="solo"
-          theme="dark"
-          hide-details
-          center-affix
-          :bg-color="iconColor(type, fieldData[index])"
-          :placeholder="placeholder"
-          v-model="fieldData[index]"
-          v-bind:readonly="!create"
-          :rules="[rules.required, rules.exists, rules.validated]"
-          required
-        >
-          {{ fieldData[index] }}
-        </v-text-field>
-        <div v-else>
-          {{ fieldData[index] }}
+      <v-chip
+        class="ma-1"
+        label
+        :color="iconColor(type, item.value)"
+        v-bind:closable="edit ? !display : undefined"
+        :close-icon="edit ? 'mdi-close' : undefined"
+        @click:close="onDeleteChip(item.value, index)"
+        :to="whereTo(item.value)"
+        v-bind:small="small"
+      >
+        <v-avatar start>
+          <v-icon>{{ iconImage(type, item.value) }}</v-icon>
+        </v-avatar>
+        <div v-if="!isObject(item.value)">
+          <v-text-field
+            v-if="edit"
+            class="m-0 input-chip"
+            theme="dark"
+            hide-details
+            single-line
+            :bg-color="iconColor(type, item.value)"
+            :placeholder="placeholder"
+            v-model="item.value"
+            v-bind:readonly="!create"
+            :rules="[rules.required, rules.exists, rules.validated]"
+            required
+          ></v-text-field>
+          <div v-else>
+            {{ item.value }}
+          </div>
         </div>
-      </div>
-      <div v-else-if="type === 'keyword'">
-        {{ item.tag }}
-      </div>
-      <div v-else-if="type === 'venue'">
-        {{ item.venue.abbrev || item.venue.title }}
-      </div>
-      <div v-else-if="type === 'role'">
-        {{ item.affiliation.person.name }}
-        <span v-if="item.affiliation.org && item.affiliation.org.name">({{ item.affiliation.org.name }})</span>
-        <span v-if="edit && item.affiliation.person && item.affiliation.person.email">({{ item.affiliation.person.email }})</span>
-      </div>
-      <div v-else-if="type === 'relation'">
-        {{ item.artifact_id }} {{ $filters.titlecase(item.relation) }}
-        {{ item.related_artifact_id }}
-      </div>
-      <div v-else-if="type === 'reverse-relation'">
-        {{ item.related_artifact_id }}
-        {{ $filters.titlecase(flipRelation(item.relation)) }}
-        {{ item.artifact_id }}
-      </div>
-    </v-chip>
-    <v-btn
-      v-if="create"
-      @click="fieldData.push('')"
-      class="ml-2 mb-2 text-success"
-      variant="tonal"
-      density="compact"
-      icon="mdi-plus"
-      :disabled="typeof this.formModel !== 'undefined' ? !formModel : false"
-    ></v-btn>
-  </div>
+        <div v-else-if="type === 'keyword'">
+          {{ item.value.tag }}
+        </div>
+        <div v-else-if="type === 'venue'">
+          {{ item.value.venue.abbrev || item.value.venue.title }}
+        </div>
+        <div v-else-if="type === 'role'">
+          {{ item.value.affiliation.person.name }}
+          <span v-if="item.value.affiliation.org && item.value.affiliation.org.name">({{ item.value.affiliation.org.name }})</span>
+          <span v-if="edit && item.value.affiliation.person && item.value.affiliation.person.email">({{ item.value.affiliation.person.email }})</span>
+        </div>
+        <div v-else-if="type === 'relation'">
+          {{ item.value.artifact_id }} {{ $filters.titlecase(item.value.relation) }}
+          {{ item.value.related_artifact_id }}
+        </div>
+        <div v-else-if="type === 'reverse-relation'">
+          {{ item.value.related_artifact_id }}
+          {{ $filters.titlecase(flipRelation(item.value.relation)) }}
+          {{ item.value.artifact_id }}
+        </div>
+      </v-chip>
+    </v-col>
+    <v-col cols="auto" class="pa-0">
+      <v-btn
+        v-if="create"
+        @click="fields.push({
+          id: Math.random().toString(36).slice(2),
+          value: ''
+        })"
+        class="text-success"
+        variant="tonal"
+        density="compact"
+        icon="mdi-plus"
+        :disabled="typeof this.formModel !== 'undefined' ? !formModel : false"
+      ></v-btn>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import { artifactIcon, artifactColor, venueIcon, reverseRelation } from '@/helpers'
+import isEqual from 'lodash.isequal';
 
 // properties are not suppose to change in vue, so we copy the object and change copy
 export default defineComponent({
@@ -139,17 +144,34 @@ export default defineComponent({
           return (typeof this.validator !== "undefined") ? this.validator(value) : true
         },
       },
-      fieldData: [],
+      fields: [],
     }
   },
-  mounted() {
-    Object.assign(this.fieldData, this.modelValue)
+  // why do we need separate id for each field instead of using index directly?
+  // Because indexes will change when we splice fields,
+  // if we use index as key, vue will display wrong data in
+  // frontend since it internally uses index as key (guess)
+  created() {
+    Object.assign(this.fields, this.modelValue.map((item) => ({
+      id: Math.random().toString(36).slice(2),
+      value: item
+    })))
   },
   watch: {
-    fieldData: {
+    modelValue: {
       deep: true,
-      handler(value) {
-        this.$emit('update:modelValue', value)
+      handler() {
+        if (isEqual(this.fields.map(e => e.value), this.modelValue)) return
+        Object.assign(this.fields, this.modelValue.map((item) => ({
+          id: Math.random().toString(36).slice(2),
+          value: item
+        })))
+      }
+    },
+    fields: {
+      deep: true,
+      handler() {
+        this.$emit('update:modelValue', this.fields.map((item) => item.value))
       }
     }
   },
@@ -193,17 +215,17 @@ export default defineComponent({
     onDeleteChip(item, index) {
       if (this.type === 'relation') 
         this.deleteRelationship(item.id)
-      this.fieldData.splice(index, 1)
+      this.fields.splice(index, 1)
     },
     async deleteRelationship(id) {
       let response = await this.$relationshipEndpoint.delete(id)
-    }
+    },
   }
 });
 </script>
 
-<style>
-.input-chip input {
-  color: transparent
+<style scoped>
+.input-chip {
+  min-width: 150px;
 }
 </style>
